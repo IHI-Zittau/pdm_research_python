f6eab8fa2d40a88ba47f0803c7bcf97c29217bf1
ruediklein <ruediklein@tialea.com>
2013-01-28 22:33:42 +0100

    saving of tagged sentences in job files along the '/' format test with the tagged corpus reader

--- pdm_research_python/pos_tagging.py
+++ pdm_research_python/pos_tagging.py
@@ -10,43 +10,60 @@ if __name__ == '__main__':
 ''' Script to tag text files and write them to an output directory '''
 
 import os
-directory = "D:/Eigene Dateien_rklein/z_Forschung/_Konferenzen/_79_ICFCA - Dresden - Concept Analysis/Data/Output/_Product_Management/"
+directory = "D:/Eigene Dateien_rklein/z_Forschung/_Konferenzen/_79_ICFCA - Dresden - Concept Analysis/Data/"
+input_directory = directory + "Input/_Product_Management/"
+output_directory = directory + "1_POS/"
+if not os.path.exists(output_directory): os.mkdir(output_directory)
 
 # reading stuff
-file_list = os.listdir(directory)
+file_list = os.listdir(input_directory)
 print file_list
 
 # just for testing create a corpus reader
 from nltk.corpus.reader import PlaintextCorpusReader
-reader = PlaintextCorpusReader(directory,'.*.txt')
-reader.fileids()
+reader = PlaintextCorpusReader(input_directory,'.*.txt')
 
+reader.fileids()
 reader.raw()
+reader.sents()
 reader.words()
 
-# tagging
-import nltk
-
 # tokenizing
-import pprint
-sent_tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
-for i in range(len(file_list)):
-    reader = PlaintextCorpusReader(directory,str(file_list[i]))
-    text = str(reader.raw)
-    sents = sent_tokenizer.tokenize(text)
-    pprint.pprint(sents)
-
-# default tagger from NLTK
+import nltk
+# import pprint
+# sent_tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
+# default POS tagger from NLTK
 pos = "nltk"
-path = directory + pos
+path = output_directory + pos
 if not os.path.exists(path): os.mkdir(path)
 for i in range(len(file_list)):
-    reader = PlaintextCorpusReader(directory,str(file_list[i]))
-    text_pos = nltk.pos_tag(reader.words()) # word tokenizer included
+#    posting = []
     output = path + "/" + str(file_list[i])
     jfile=open (output,"w")
-    try:
-        jfile.write(str(text_pos))
-    finally:
-        jfile.close()
-
+    reader = PlaintextCorpusReader(input_directory,str(file_list[i]))
+    text = str(reader.raw())
+    sents = nltk.sent_tokenize(text)
+#    print sents
+    for sent in sents:
+        sentence = []
+        tokens = nltk.word_tokenize(sent)
+#        print tokens
+        text_pos = nltk.pos_tag(tokens)
+#        print " ".join(word+"/"+tag for word, tag in text_pos)
+        jfile.write(str(" ".join(word+"/"+tag for word, tag in text_pos)))
+        jfile.write("\n")
+    jfile.close()
+                            
+        
+# Check, whether the format of the tagged postings is good for the tagged corpus reader
+# p.51 NLTK Cookbook
+input_directory = path
+from nltk.corpus.reader import TaggedCorpusReader
+reader = TaggedCorpusReader(input_directory, r'.*\.txt')
+reader.words()
+# ['The', 'expense', 'and', 'time', 'involved', 'are', ...]
+reader.tagged_words()
+reader.sents()
+reader.tagged_sents()
+reader.paras()
+reader.tagged_paras()
